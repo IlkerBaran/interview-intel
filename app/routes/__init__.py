2416b52c7ba8f1e4baebc80bdc855f43d4408d0e
IlkerBaran <ilkerbaran01@gmail.com>
2026-03-20 01:01:33 -0700

    feat: set up base routes and centralized blueprint registration
    - add main and error routes
    - centralize blueprint registration in routes/__init__.py

---
+++ app/routes/__init__.py
@@ -0,0 +1,20 @@
+from .main import main
+from .messages import messages_bp
+from .dashboard import dashboard_bp
+from .tasks import tasks_bp
+from .auth import auth_bp
+from .errors import errors_bp
+
+def register_blueprints(app):
+    """
+    Register blueprints
+
+    Gathers and connects to app all the blueprints
+    to import in app/__init__.py
+    """
+    app.register_blueprint(main)
+    app.register_blueprint(messages_bp)
+    app.register_blueprint(dashboard_bp)
+    app.register_blueprint(tasks_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(errors_bp)
