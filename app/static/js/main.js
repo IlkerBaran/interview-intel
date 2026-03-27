bbc6b95f9875a56b85500ae8a73f8d4a7709d35c
IlkerBaran <ilkerbaran01@gmail.com>
2026-03-27 00:36:12 -0700

    feat(main.js): add password toggle

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,4 @@
+// navbar toggle
 const toggle = document.getElementById("nav-toggle");
 const nav = document.getElementById("nav-right");
 
@@ -6,3 +7,19 @@ if (toggle && nav) {
         nav.classList.toggle("open");
     });
 }
+
+
+// password toggle
+document.querySelectorAll(".password-toggle").forEach(button => {
+    button.addEventListener("click", () => {
+        const input = button.previousElementSibling;
+
+        if (input.type === "password") {
+            input.type = "text";
+            button.textContent = "Hide";
+        } else {
+            input.type = "password";
+            button.textContent = "Show";
+        }
+    });
+});
